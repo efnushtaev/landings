a2f31af9a3ed29ec5abdac7e693fda122609211b
efnushtaev <efnushtaev@gmail.com>
2019-06-27 21:17:33 +0300

    added shots

--- Landing__3/send.php
+++ Landing__3/send.php
@@ -17,3 +17,23 @@ if((isset($_POST['user'])&&$_POST['user']!="")&&(isset($_POST['email'])&&$_POST[
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 }
 ?>
+
+
+
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
